10b589f8e2a50683041fe52860ad55591800d0dc
Jihwan Seo <jseo11@ucsc.edu>
2017-01-21 10:58:39 -0800

    image

--- app/src/main/java/com/example/baiwen/vcloset/MainActivity.java
+++ app/src/main/java/com/example/baiwen/vcloset/MainActivity.java
@@ -152,4 +152,14 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+
 }
--- app/src/main/java/com/example/baiwen/vcloset/PhotoViewer.java
+++ app/src/main/java/com/example/baiwen/vcloset/PhotoViewer.java
@@ -10,4 +10,6 @@ public class PhotoViewer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_viewer);
     }
+
+
 }
