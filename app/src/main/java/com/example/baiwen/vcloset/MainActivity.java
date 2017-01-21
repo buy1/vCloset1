2f0553a9cfc23425801c2f47e8c8a7bb09d3c192
buy1 <bhuang21@ucsc.edu>
2017-01-21 01:45:14 -0800

    removed tools from content_main.xml

--- app/src/main/java/com/example/baiwen/vcloset/MainActivity.java
+++ app/src/main/java/com/example/baiwen/vcloset/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(takePicture);
+
             }
         });
         FloatingActionButton importPhotoButton = (FloatingActionButton) findViewById(R.id.floatingActionButton5);
