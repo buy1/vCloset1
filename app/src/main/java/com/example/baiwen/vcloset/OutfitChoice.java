bfa6e7d71b05fa8bca3e8cbab09d30a1050c9182
Justin Seo <Justin@dhcp-169-233-172-226.cruznet.ucsc.edu>
2017-01-21 02:39:52 -0800

    Buttons

---
+++ app/src/main/java/com/example/baiwen/vcloset/OutfitChoice.java
@@ -0,0 +1,13 @@
+package com.example.baiwen.vcloset;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OutfitChoice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_outfit_choice);
+    }
+}
