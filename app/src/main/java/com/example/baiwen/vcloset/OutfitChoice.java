dbd6358e6a598605836c26564e33249b31c8bf08
SotaNakajima <snakajim@ucsc.edu>
2017-01-21 04:11:03 -0800

    test

--- app/src/main/java/com/example/baiwen/vcloset/OutfitChoice.java
+++ app/src/main/java/com/example/baiwen/vcloset/OutfitChoice.java
@@ -1,13 +1,25 @@
 package com.example.baiwen.vcloset;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class OutfitChoice extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_outfit_choice);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(new CustomPagerAdapter(this));
+
     }
+
+
 }
